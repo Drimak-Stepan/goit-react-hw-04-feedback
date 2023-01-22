b47e225707e24dfa346766a74dbc2be453024c9e
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-01-22 14:28:15 +0200

    Pro

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -12,7 +12,7 @@ const Section = ({ children, title }) => {
 
 export default Section;
 
-Section.PropTypes = {
+Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
 };
